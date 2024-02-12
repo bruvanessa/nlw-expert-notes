31b7b1b0c56e064103bb8e27b3b4fc11d08cf8ba
bruvanessa <brunavanessa.n@gmail.com>
2024-02-12 10:52:12 -0300

    aula 3

--- src/app.tsx
+++ src/app.tsx
@@ -32,6 +32,15 @@ function onNoteCreated (content: string) {
   localStorage.setItem("notes", JSON.stringify(notesArray))
 }
 
+function onNoteDeleted (id: string) {
+  const notesArray = notes.filter(note => {
+    return note.id !== id
+})
+  setNotes(notesArray)
+
+  localStorage.setItem("notes", JSON.stringify(notesArray))
+}
+
  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
   const query = event.target.value
 
@@ -43,7 +52,7 @@ function onNoteCreated (content: string) {
   : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 md:px-0">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert"/>
 
       <form className="w-full">
@@ -56,12 +65,12 @@ function onNoteCreated (content: string) {
       </form>
 
       <div className="h-px bg-slate-700"></div>
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
       
         <NewNoteCard onNoteCreated={onNoteCreated}/>
 
       {filteredNotes.map(note => {
-        return <NoteCard key={note.id} note={note}/>
+        return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
       })}
 
       </div>
